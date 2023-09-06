6944543d10dec5ac3868c0e4e70b2382bb6322b2
FrontEndFlame <138845666+FrontEndFlame@users.noreply.github.com>
2023-09-06 23:02:05 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,243 @@
+document.addEventListener('DOMContentLoaded', function() {
+  
+// Скролл заголовков
+var heroTitles = document.querySelectorAll('.hero-titles');
+var rmContainer = document.getElementById('works');
+
+heroTitles.forEach(function(title) {
+  title.addEventListener('click', function() {
+    rmContainer.style.opacity = '1';
+    window.scrollTo({
+      top: rmContainer.offsetTop,
+      behavior: 'smooth' // Плавный переход
+  });
+  });
+});
+
+// var heroTitle = document.getElementById('hero__title');
+// var heroScrollDiv = document.getElementById('hero__vertical-scroll');
+
+// var heroWords = document.getElementsByClassName('hero__word');
+
+// var heroContainerHeight = heroTitle.clientHeight;
+// var totalHeight = 0;
+
+// for (var i = 0; i < heroWords.length; i++) {
+//   totalHeight += heroWords[i].offsetHeight;
+
+//   if (totalHeight > heroContainerHeight) {
+//     heroScrollDiv.style.height = totalHeight + 'px';
+//     break;
+//   }
+// }
+
+
+// Форма с заявкой
+const showFormBtn = document.getElementsByClassName('show-form-btn');
+const formContainer = document.getElementById('form');
+const closeFormBtn = document.getElementById('close-form-btn');
+
+for (let i = 0; i < showFormBtn.length; i++) {
+  showFormBtn[i].addEventListener('click', () => {
+    formContainer.style.display = 'block';
+  });
+}
+
+closeFormBtn.addEventListener('click', () => {
+  formContainer.style.display = 'none';
+});
+
+    
+// Появление контента
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 10;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+var requestId = null;
+
+function loop() {
+  requestId = null;
+  reveal();
+  if (!requestId) {
+    requestId = requestAnimationFrame(loop);
+  }
+}
+
+window.addEventListener("load", function() {
+  requestId = requestAnimationFrame(loop);
+});
+
+
+// Услуги
+const servicesItems = document.querySelectorAll(".services__accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (let i = 0; i < servicesItems.length; i++) {
+    servicesItems[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+for (let i = 0; i < servicesItems.length; i++) {
+  servicesItems[i].addEventListener('click', toggleAccordion);
+}
+
+// Преимущества
+const advantages = document.querySelectorAll(".sub__accordion button");
+
+function toggleAdvantagesAccordion() {
+  const avantagesToggle = this.getAttribute('aria-expanded');
+  
+  for (let i = 0; i < advantages.length; i++) {
+    advantages[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (avantagesToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+for (let i = 0; i < advantages.length; i++) {
+  advantages[i].addEventListener('click', toggleAdvantagesAccordion);
+}
+
+
+// Как мы помогли клиентам 
+const helpedBtn1 = document.querySelector('#helped-button-1');
+const helpedBtn2 = document.querySelector('#helped-button-2');
+const helpedBtn3 = document.querySelector('#helped-button-3');
+const helpedBtn4 = document.querySelector('#helped-button-4');
+const helpedBtn5 = document.querySelector('#helped-button-5');
+const helpedContent1 = document.querySelector('#helped__more-block-1');
+const helpedContent2 = document.querySelector('#helped__more-block-2');
+const helpedContent3 = document.querySelector('#helped__more-block-3');
+const helpedContent4 = document.querySelector('#helped__more-block-4');
+const helpedContent5 = document.querySelector('#helped__more-block-5');
+const helpedBlocks = document.querySelectorAll('.helped__block');
+const helpedTitle = document.getElementById('helped-main-title');
+const contents = document.querySelectorAll('.helped__more-content');
+
+helpedBtn1.addEventListener('click', () => {
+  if (helpedContent1.style.display === 'block') {
+    // Скрыть контент
+    hideContent(helpedContent1);
+    // Прокрутить страницу к helped-title
+    helpedTitle.scrollIntoView({ behavior: 'smooth' });
+  } else {
+    showContent(helpedContent1);
+    helpedBtn1.classList.add("helped__block-open");
+    helpedBtn2.classList.remove("helped__block-open");
+    helpedBtn3.classList.remove("helped__block-open");
+    helpedBtn4.classList.remove("helped__block-open");
+  }
+});
+
+helpedBtn2.addEventListener('click', () => {
+  if (helpedContent2.style.display === 'block') {
+    // Скрыть контент
+    hideContent(helpedContent2);
+    // Прокрутить страницу к helped-title
+    helpedTitle.scrollIntoView({ behavior: 'smooth' });
+  } else {
+    showContent(helpedContent2);
+    helpedBtn1.classList.remove("helped__block-open");
+    helpedBtn2.classList.add("helped__block-open");
+    helpedBtn3.classList.remove("helped__block-open");
+    helpedBtn4.classList.remove("helped__block-open");
+  }
+});
+
+helpedBtn3.addEventListener('click', () => {
+  if (helpedContent3.style.display === 'block') {
+    // Скрыть контент
+    hideContent(helpedContent3);
+    // Прокрутить страницу к helped-title
+    helpedTitle.scrollIntoView({ behavior: 'smooth' });
+  } else {
+    showContent(helpedContent3);
+    helpedBtn1.classList.remove("helped__block-open");
+    helpedBtn2.classList.remove("helped__block-open");
+    helpedBtn3.classList.add("helped__block-open");
+    helpedBtn4.classList.remove("helped__block-open");
+  }
+});
+
+helpedBtn4.addEventListener('click', () => {
+  if (helpedContent4.style.display === 'block') {
+    // Скрыть контент
+    hideContent(helpedContent4);
+    // Прокрутить страницу к helped-title
+    helpedTitle.scrollIntoView({ behavior: 'smooth' });
+  } else {
+    showContent(helpedContent4);
+    helpedBtn1.classList.remove("helped__block-open");
+    helpedBtn2.classList.remove("helped__block-open");
+    helpedBtn3.classList.remove("helped__block-open");
+    helpedBtn4.classList.add("helped__block-open");
+  }
+});
+
+function showContent(content) {
+  helpedBlocks.forEach((block) => {
+    block.classList.add("helped__block-open");
+  });
+  
+
+  // Скрытие всех контентов
+  contents.forEach((c) => {
+    c.style.display = 'none';
+  });
+  
+  // Показываем выбранный контент
+  content.style.display = 'block';
+}
+
+function hideContent(content) {
+  console.log('lol');
+  helpedBlocks.forEach((block) => {
+    block.classList.remove("helped__block-open");
+  });
+  
+
+  // Скрытие всех контентов
+  contents.forEach((c) => {
+    c.style.display = 'none';
+  });
+
+  content.style.display = 'none';
+}
+
+
+
+
+// Перезагрузка страницы при первом запуске
+$(document).ready(function() {
+  // Проверяем, была ли страница уже загружена ранее на этом устройстве
+  if (localStorage.getItem('pageLoaded') === null) {
+    // Если страница не была загружена ранее на этом устройстве, устанавливаем флаг в локальном хранилище
+    localStorage.setItem('pageLoaded', 'true');
+    
+    // Запускаем таймер для перезагрузки через три секунды
+    setTimeout(function() {
+      location.reload();
+    }, 300);
+  }
+});
+});
---
+++ js/swiper.js
@@ -0,0 +1,170 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  const heroSider = new Swiper('.hero__swiper', {
+    direction: 'vertical',
+    loop: true,
+    spaceBetween: 25,
+    speed: 1000,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false // Продолжать автопрокрутку после взаимодействия пользователя с слайдером
+    }
+  });
+
+  
+  const subSlider = new Swiper('.sub__swiper', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    spaceBetween: 150,
+    parallax: true,
+    speed: 500,
+    allowTouchMove: false
+  });
+
+  const subBtn1 = document.querySelector('#sub-nav-li-1');
+  const subBtn2 = document.querySelector('#sub-nav-li-2');
+  // const subBtn3 = document.querySelector('#sub-nav-li-3');
+  // const subBtn4 = document.querySelector('#sub-nav-li-4');
+  const subBtns = document.querySelectorAll('.sub-nav-list li');
+  const subLineA = document.querySelector('.sub-nav-line-a');
+  const subLineB = document.querySelector('.sub-nav-line-b');
+  const subContainer = document.querySelector('.sub__container');
+  
+  subBtn1.addEventListener('click', () => {
+    subBtn1.classList.add("active");
+    subBtn2.classList.remove("active");
+    // subBtn3.classList.remove("active");
+    // subBtn4.classList.remove("active");
+    subSlider.slideTo(0);
+  });
+
+  subBtn2.addEventListener('click', () => {
+    subBtn1.classList.remove("active");
+    subBtn2.classList.add("active");
+    // subBtn3.classList.remove("active");
+    // subBtn4.classList.remove("active");
+    subSlider.slideTo(1);
+  });
+
+  // subBtn3.addEventListener('click', () => {
+  //   subBtn1.classList.remove("active");
+  //   subBtn2.classList.remove("active");
+  //   subBtn3.classList.add("active");
+  //   subBtn4.classList.remove("active");
+  //   subSlider.slideTo(2);
+  // });
+
+  // subBtn4.addEventListener('click', () => {
+  //   subBtn1.classList.remove("active");
+  //   subBtn2.classList.remove("active");
+  //   subBtn3.classList.remove("active");
+  //   subBtn4.classList.add("active");
+  //   subSlider.slideTo(3);
+  // });
+  
+  subBtns.forEach(item => {
+    const subLineBWidth = item.offsetWidth;
+    subLineB.style.width = `${subLineBWidth}px`;
+
+    item.addEventListener('mouseenter', () => {
+      const itemLeft = item.getBoundingClientRect().left;
+      const itemWidth = item.offsetWidth;
+          
+      subLineA.style.left = `${itemLeft}px`;
+      subLineA.style.width = `${itemWidth}px`;
+      subLineA.style.opacity = '1';
+    });
+    item.addEventListener('mouseleave', () => {
+      subLineA.style.opacity = '0';
+    });
+    item.addEventListener('click', () => {
+      const itemLeft = item.getBoundingClientRect().left;
+      subLineB.style.left = `${itemLeft}px`;
+    });
+  });
+
+  const cargoSlider = new Swiper('.helped-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    initialSlide: 3,
+    allowTouchMove: false,
+    preloadSlides: 1,
+    breakpoints: {
+      670: {
+        slidesPerView: 3,
+      }
+    },
+    spaceBetween: 34,
+    centeredSlides: true,
+    effect: 'coverflow',
+    coverflowEffect: {
+      rotate: -20,
+      slideShadows: false,
+    },
+    navigation: {
+      nextEl: '.helped .swiper-button-next',
+      prevEl: '.helped .swiper-button-prev',
+    }
+  });
+
+  var helpedSliderContainer = document.querySelector('.helped .swiper-wrapper');
+  helpedSliderContainer.addEventListener('click', function(event) {
+    var btn = event.target.closest('.helped-button');
+    if (btn) {
+      var slidePrev = btn.closest('.swiper-slide-prev');
+      var slideCenter = btn.closest('.swiper-slide-active');
+      var slideNext = btn.closest('.swiper-slide-next');
+      var prevSlide = slidePrev ? slidePrev.previousElementSibling : null;
+      var nextSlide = slideNext ? slideNext.nextElementSibling : null;
+      if (nextSlide) {
+        cargoSlider.slideNext(); // Переход к предыдущему слайду
+      } else if (prevSlide) {
+        cargoSlider.slidePrev();
+      }
+      if (!slideCenter) {
+        setTimeout(function() {
+          cargoSlider.slideNext();
+        }, 100);
+      }
+    }
+  });
+
+  // Получаем кнопки навигации
+  const prevScrollButton = document.querySelector('.prev-scrll');
+  const nextScrollButton = document.querySelector('.next-scrll');
+
+  // Получаем заголовок
+  const helpedMainTitle = document.getElementById('helped-main-title');
+
+  // Добавляем обработчик события клика на кнопку swiper-button-prev
+  prevScrollButton.addEventListener('click', () => {
+    helpedMainTitle.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  // Добавляем обработчик события клика на кнопку swiper-button-next
+  nextScrollButton.addEventListener('click', () => {
+    helpedMainTitle.scrollIntoView({ behavior: 'smooth' });
+  });
+
+  // Функция для скролла к элементу
+  function scrollToElement(element) {
+    window.scrollTo({
+      top: element.offsetTop,
+      behavior: 'smooth'
+    });
+  }
+
+  cargoSlider.on('slideChange', function () {
+    var activeSlide = cargoSlider.slides[cargoSlider.activeIndex];
+
+    // Скрываем контент на неактивных слайдах
+    cargoSlider.slides.forEach(function (slide) {
+      if (slide !== activeSlide) {
+        slide.querySelector('.helped__more-content').style.display = 'none';
+      }
+    });
+  });
+
+});
